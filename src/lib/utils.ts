010c411ee0871a25676586f98feef31006209da9
yigityalim <mehmetyalm@icloud.com>
2024-05-11 17:07:06 +0300

    eklemeler düzeltildi.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,4 @@
 import { type ClassValue, clsx } from 'clsx'
-import _ from 'lodash'
-import React from 'react'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
